08f596f29338c9120ddf2213daf50f0a88438415
Danizzz0404 <daniduhettiarachchi0404@gmail.com>
2025-05-29 23:34:13 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,44 @@
+const CACHE_NAME = 'survival-game-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/game.js',
+  '/manifest.json',
+  '/girl.png',
+  '/icon-192.png',
+  '/icon-512.png'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('survival-game').then((cache) => {
-      return cache.addAll([
-        'index.html',
-        'game.js',
-        'manifest.json'
-      ]);
-    })
+// Install the service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Update service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList =>
+      Promise.all(keyList.map(key => {
+        if (!cacheWhitelist.includes(key)) {
+          return caches.delete(key);
+        }
+      }))
+    )
   );
 });
